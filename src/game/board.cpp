ce3602cdca03399b31c01b0b30bf04d78882bfc9
marcus.koseck <marcuskoseck@gmail.com>
2025-07-23 21:00:16 -0500

    trying to understand the file structure and how to make custom files.

---
+++ src/game/board.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void make_board(){
+    cout<<"This is a test for using this function";
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+#include "game/board.cpp"
+
+main(){
+    make_board();
+}
